019a05e5255e7baa8b940e9a37d25527b5984040
Feroze Naina <ferozenaina@gmail.com>
2014-07-06 14:52:40 +0530

    chapter 1 example

---
+++ raptor/raptor.cpp
@@ -0,0 +1,121 @@
+/***************************************************************************
+
+    file                 : raptor.cpp
+    created              : Sun Jul 6 12:03:06 IST 2014
+    copyright            : (C) 2002 Feroze Naina
+
+ ***************************************************************************/
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h> 
+#include <math.h>
+
+#include <tgf.h> 
+#include <track.h> 
+#include <car.h> 
+#include <raceman.h> 
+#include <robottools.h>
+#include <robot.h>
+
+#include <iostream>
+using namespace std;
+
+
+
+static tTrack	*curTrack;
+
+static void initTrack(int index, tTrack* track, void *carHandle, void **carParmHandle, tSituation *s); 
+static void newrace(int index, tCarElt* car, tSituation *s); 
+static void drive(int index, tCarElt* car, tSituation *s); 
+static void endrace(int index, tCarElt *car, tSituation *s);
+static void shutdown(int index);
+static int  InitFuncPt(int index, void *pt);
+
+static int gate = 0;
+
+
+/* 
+ * Module entry point  
+ */ 
+extern "C" int 
+raptor(tModInfo *modInfo) 
+{
+    memset(modInfo, 0, 10*sizeof(tModInfo));
+
+    modInfo->name    = strdup("raptor");		/* name of the module (short) */
+    modInfo->desc    = strdup("");	/* description of the module (can be long) */
+    modInfo->fctInit = InitFuncPt;		/* init function */
+    modInfo->gfId    = ROB_IDENT;		/* supported framework version */
+    modInfo->index   = 1;
+
+    return 0; 
+} 
+
+/* Module interface initialization. */
+static int 
+InitFuncPt(int index, void *pt) 
+{ 
+    tRobotItf *itf  = (tRobotItf *)pt; 
+
+    itf->rbNewTrack = initTrack; /* Give the robot the track view called */ 
+				 /* for every track change or new race */ 
+    itf->rbNewRace  = newrace; 	 /* Start a new race */
+    itf->rbDrive    = drive;	 /* Drive during race */
+    itf->rbPitCmd   = NULL;
+    itf->rbEndRace  = endrace;	 /* End of the current race */
+    itf->rbShutdown = shutdown;	 /* Called before the module is unloaded */
+    itf->index      = index; 	 /* Index used if multiple interfaces */
+    return 0; 
+} 
+
+/* Called for every track change or new race. */
+static void  
+initTrack(int index, tTrack* track, void *carHandle, void **carParmHandle, tSituation *s) 
+{ 
+    curTrack = track;
+    *carParmHandle = NULL; 
+} 
+
+/* Start a new race. */
+static void  
+newrace(int index, tCarElt* car, tSituation *s) 
+{ 
+} 
+
+/* Drive during race. */
+static void
+drive(int index, tCarElt* car, tSituation *s)
+{
+    memset(&car->ctrl, 0, sizeof(tCarCtrl));
+
+    float angle;
+    const float SC = 1.0;
+
+    angle = RtTrackSideTgAngleL(&(car->_trkPos)) - car->_yaw;
+    NORM_PI_PI(angle); // put the angle back in the range from -PI to PI
+    angle -= SC*car->_trkPos.toMiddle/car->_trkPos.seg->width;
+
+    // set up the values to return
+    car->ctrl.steer = angle / car->_steerLock;
+    car->ctrl.gear = 1; // first gear
+    car->ctrl.accelCmd = 0.3; // 30% accelerator pedal
+    car->ctrl.brakeCmd = 0.0; // no brakes
+}
+
+/* End of the current race */
+static void
+endrace(int index, tCarElt *car, tSituation *s)
+{
+}
+
+/* Called before the module is unloaded */
+static void
+shutdown(int index)
+{
+}
+
